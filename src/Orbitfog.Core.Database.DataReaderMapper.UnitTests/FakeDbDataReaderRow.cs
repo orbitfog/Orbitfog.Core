0b3db173448e0d450ac539360dc893dbfb9dc424
orbitfog <65041071+orbitfog@users.noreply.github.com>
2020-05-17 22:39:25 +0200

    net5, c#9

--- src/Orbitfog.Core.Database.DataReaderMapper.UnitTests/FakeDbDataReaderRow.cs
+++ src/Orbitfog.Core.Database.DataReaderMapper.UnitTests/FakeDbDataReaderRow.cs
@@ -1,53 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Orbitfog.Core.Database.DataReaderMapper.UnitTests
 {
     public class FakeDbDataReaderRow
     {
-        private readonly Dictionary<string, object?> nameValueList;
-        private readonly Dictionary<int, object?> ordinalValueList;
-        private readonly Dictionary<int, string> ordinalNameList;
         private readonly Dictionary<string, int> nameOrdinalList;
         private readonly Dictionary<int, Type> typeList;
+        private readonly Dictionary<int, bool> isDBNullList;
+        private readonly Dictionary<int, object> ordinalValueList;
 
         public FakeDbDataReaderRow()
         {
-            nameValueList = new Dictionary<string, object?>();
-            ordinalValueList = new Dictionary<int, object?>();
-            ordinalNameList = new Dictionary<int, string>();
             nameOrdinalList = new Dictionary<string, int>();
             typeList = new Dictionary<int, Type>();
+            isDBNullList = new Dictionary<int, bool>();
+            ordinalValueList = new Dictionary<int, object>();
         }
 
         public FakeDbDataReaderRow AddColumn(Type type, string name, object? value)
         {
-            int ordinal = nameValueList.Count;
+            int ordinal = nameOrdinalList.Count;
 
-            nameValueList.Add(name, value);
-            ordinalValueList.Add(ordinal, value);
-            ordinalNameList.Add(ordinal, name);
             nameOrdinalList.Add(name, ordinal);
             typeList.Add(ordinal, type);
+            isDBNullList.Add(ordinal, value == null);
+            if (value != null)
+            {
+                ordinalValueList.Add(ordinal, value);
+            }
 
             return this;
         }
 
-        public object? GetValue(int ordinal)
+        public object GetValue(int ordinal)
         {
             return ordinalValueList[ordinal];
         }
 
-        public object? GetValue(string name)
+        public object GetValue(string name)
         {
-            return nameValueList[name];
+            return ordinalValueList[nameOrdinalList[name]];
         }
 
-        public int FieldCount => ordinalValueList.Count;
+        public int FieldCount => nameOrdinalList.Count;
 
         public string GetName(int ordinal)
         {
-            return ordinalNameList[ordinal];
+            return nameOrdinalList.FirstOrDefault(p => p.Value == ordinal).Key;
         }
 
         public Type GetType(int ordinal)
@@ -58,6 +59,11 @@ namespace Orbitfog.Core.Database.DataReaderMapper.UnitTests
         public int GetOrdinal(string name)
         {
             return nameOrdinalList[name];
+        }
+
+        public bool IsDBNull(int ordinal)
+        {
+            return isDBNullList[ordinal];
         }
     }
 }
